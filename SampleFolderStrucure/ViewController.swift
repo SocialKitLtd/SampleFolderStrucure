a4b5f60a0935fc5bc2358ce3e898320f23b4ea60
Roi Mulia <41643229+SocialKitLtd@users.noreply.github.com>
2020-12-23 16:41:58 +0200

    Initial Commit

---
+++ SampleFolderStrucure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleFolderStrucure
+//
+//  Created by Roi Mulia on 23/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
